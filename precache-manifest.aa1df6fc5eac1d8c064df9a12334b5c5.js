0268b690084a88d543335e5217d01af6593548d4
Putra <raihanputraramd@gmail.com>
2020-08-13 21:44:01 +0700

    Updates

---
+++ precache-manifest.aa1df6fc5eac1d8c064df9a12334b5c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cef8a9793f6208a105f7fc463b55e28",
+    "url": "/my-portofolio/index.html"
+  },
+  {
+    "revision": "851ce2489595775a6843",
+    "url": "/my-portofolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "91161ab47955689e4d35",
+    "url": "/my-portofolio/static/css/main.8f46407c.chunk.css"
+  },
+  {
+    "revision": "851ce2489595775a6843",
+    "url": "/my-portofolio/static/js/2.be4d81f2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-portofolio/static/js/2.be4d81f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91161ab47955689e4d35",
+    "url": "/my-portofolio/static/js/main.e17a1bf9.chunk.js"
+  },
+  {
+    "revision": "857ac466d343684d194b",
+    "url": "/my-portofolio/static/js/runtime-main.48e8baff.js"
+  }
+]);
